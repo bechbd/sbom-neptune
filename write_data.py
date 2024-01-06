09fac8df9a29ed1de7f55267a5e3e9abf28e6930
Dave Bechberger <dbechbe@amazon.com>
2024-01-05 15:08:16 -0900

    Added batching to writing nodes and rels, dropped time from 554 seconds to 1.5 seconds

--- write_data.py
+++ write_data.py
@@ -2,14 +2,17 @@ import boto3
 import json
 import logging
 from timer import benchmark_timer, Timer
+from itertools import islice
 
 
 logger = logging.getLogger(__name__)
+timer = Timer()
 
 
 class NeptuneAnalyticsSBOMWriter:
     client = None
     graph_identifier = None
+    batch_size = 200
 
     def __init__(self, graph_identifier: str, region: str) -> None:
         self.client = boto3.client("neptune-graph", region_name=region)
@@ -22,12 +25,12 @@ class NeptuneAnalyticsSBOMWriter:
         if "components" in bom:
             logging.info("Writing components")
             self.write_components(bom["components"], parent_component_id)
-        """if "dependencies" in bom:
+        if "dependencies" in bom:
             logging.info("Writing dependencies")
             self.write_dependencies(bom["dependencies"], parent_component_id)
         if "vulnerabilities" in bom:
             logging.info("Writing vulnerabilities")
-            self.write_vulnerabilities(bom["vulnerabilities"])"""
+            self.write_vulnerabilities(bom["vulnerabilities"])
 
     def write_bom(self, bom):
         parent_component_id = f"component_{bom['metadata']['component']['name']}"
@@ -56,34 +59,36 @@ class NeptuneAnalyticsSBOMWriter:
 
     @benchmark_timer
     def write_components(self, components: list, parent_component_id: str):
-        timer = Timer()
-        timer.start()
         self.__write_objects(components, "component", "name")
-        timer.stop()
         part_of_edges = [
             {"fromId": parent_component_id, "toId": f"component_{c['name']}"}
             for c in components
         ]
         logging.info("Writing PART_OF edges")
-        timer.start()
         self.__write_rel(part_of_edges, "PART_OF")
-        timer.stop()
 
         logging.info("Writing component -> externalReferences")
+        refs = []
+        refers_to_edges = []
         for c in components:
             if "externalReferences" in c:
-                self.__write_objects(
-                    c["externalReferences"], "externalReference", "url"
+                # self.__write_objects(
+                #    c["externalReferences"], "externalReference", "url"
+                # )
+                refs.extend(c["externalReferences"])
+                refers_to_edges.extend(
+                    [
+                        {
+                            "fromId": f"component_{c['name']}",
+                            "toId": f"externalReference_{r['url']}",
+                        }
+                        for r in c["externalReferences"]
+                    ]
                 )
-                refers_to_edges = [
-                    {
-                        "fromId": f"component_{c['name']}",
-                        "toId": f"externalReference_{r['url']}",
-                    }
-                    for r in c["externalReferences"]
-                ]
-                self.__write_rel(refers_to_edges, "REFERS_TO")
+        self.__write_objects(refs, "externalReference", "url")
+        self.__write_rel(refers_to_edges, "REFERS_TO")
 
+    @benchmark_timer
     def write_dependencies(self, dependencies: list, parent_component_id: str):
         self.__write_objects(dependencies, "dependency", "ref")
         uses_edges = [
@@ -92,21 +97,30 @@ class NeptuneAnalyticsSBOMWriter:
         ]
         logging.info("Writing USES edges")
         self.__write_rel(uses_edges, "USES")
+        deps = []
+        depends_on_edges = []
         for d in dependencies:
             if "dependsOn" in d:
-                deps = [{"ref": dep} for dep in d["dependsOn"]]
-                self.__write_objects(deps, "dependency", "ref")
-                depends_on_edges = [
-                    {
-                        "fromId": f"dependency_{d['ref']}",
-                        "toId": f"dependency_{dep}",
-                    }
-                    for dep in d["dependsOn"]
-                ]
-                self.__write_rel(depends_on_edges, "DEPENDS_ON")
+                deps.extend([{"ref": dep} for dep in d["dependsOn"]])
+                depends_on_edges.extend(
+                    [
+                        {
+                            "fromId": f"dependency_{d['ref']}",
+                            "toId": f"dependency_{dep}",
+                        }
+                        for dep in d["dependsOn"]
+                    ]
+                )
+
+        self.__write_objects(deps, "dependency", "ref")
+        self.__write_rel(depends_on_edges, "DEPENDS_ON")
 
     def write_vulnerabilities(self, vulnerabilities: list):
         self.__write_objects(vulnerabilities, "vulnerability", "id")
+
+    def chunk(arr_range, arr_size):
+        arr_range = iter(arr_range)
+        return iter(lambda: tuple(islice(arr_range, arr_size)), ())
 
     def __write_objects(self, objs: object, label: str, keyName: str):
         params = []
@@ -123,6 +137,7 @@ class NeptuneAnalyticsSBOMWriter:
                 SET """
             + self.__create_property_map_str(objs[0])
         )
+
         for o in objs:
             params.append(
                 {
@@ -131,7 +146,13 @@ class NeptuneAnalyticsSBOMWriter:
                 }
             )
 
-        self.__execute_query(label, {"props": params}, query)
+        arr_range = iter(params)
+        chunks = iter(lambda: tuple(islice(arr_range, self.batch_size)), ())
+        for chunk in chunks:
+            # This should not be needed but due to an issue with duplicate maps we have to guarantee uniqeness
+            res = list(map(dict, set(tuple(sorted(sub.items())) for sub in chunk)))
+
+            self.__execute_query(label, {"props": res}, query)
 
     def __write_rel(self, rels: list, label: str):
         logging.info(f"Writing {label}s")
@@ -148,7 +169,12 @@ class NeptuneAnalyticsSBOMWriter:
             + """]->(to) """
         )
 
-        self.__execute_query(label, {"rels": rels}, query)
+        arr_range = iter(rels)
+        chunks = iter(lambda: tuple(islice(arr_range, self.batch_size)), ())
+        for chunk in chunks:
+            # This should not be needed but due to an issue with duplicate maps we have to guarantee uniqeness
+            res = list(map(dict, set(tuple(sorted(sub.items())) for sub in chunk)))
+            self.__execute_query(label, {"rels": res}, query)
 
     def __execute_query(self, label, params, query):
         resp = self.client.execute_open_cypher_query(
